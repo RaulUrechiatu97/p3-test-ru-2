9b85b12dd88220c87bb1c2c4aebe091a44adc13f
Raul Urechiatu <raulurechiatu@gmail.com>
2020-11-19 16:54:35 +0200

    Added the main class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static String exB (List<StreamingPlatform> platforms) {
 
+	}
+	
+	public static List<StreamingPlatform> exC(List<StreamingPlatform> devices) {
+
+	}
+	
+	public static List<StreamingPlatform> exD(List<StreamingPlatform> devices) {
+
+	}
+	
+	public static Map<Content, List<StreamingPlatform>> exE(List<StreamingPlatform> devices) {
+
+	}
+	
+	public static void main(String[] args) {
+
+		
+		System.out.println(exB(platforms));
+		
+		System.out.println(exC(platforms));
+		
+		System.out.println(exD(platforms));
+		
+		System.out.println(exE(platforms));
 	}
 
 }
